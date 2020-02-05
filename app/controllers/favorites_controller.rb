9c6e730413be97d8e1306748adefc3c426ff3a41
keita <onini21@gmail.com>
2020-02-05 10:07:42 +0000

    いいね機能修正。

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,12 @@ class FavoritesController < ApplicationController
 		content = Content.find(params[:content_id])
 		favorite = current_user.favorites.new(content_id: content.id)
 		favorite.save
-		redirect_back(fallback_location: content_path)
+		redirect_to content_path(content)
 	end
 	def destroy
 		content = Content.find(params[:id])
 		favorite = current_user.favorites.find_by(content_id: content.id)
 		favorite.destroy
-		redirect_back(fallback_location: content_path)
+		redirect_to content_path(content)
 	end
 end
