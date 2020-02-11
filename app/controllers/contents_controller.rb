da2327f1759b6e74257befae65075f934a6e78a3
keita <onini21@gmail.com>
2020-02-11 03:25:35 +0000

    　いいね機能を非同期化

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,8 +2,14 @@ class ContentsController < ApplicationController
 
   def index
   	@genres = Genre.all
-  	@contents = Content.search(params[:search]).order(created_at: :desc)
-  end
+    if params[:search]
+      @contents = Content.search(params[:search]).order(created_at: :desc)
+    elsif params[:genre]
+      @contents = Content.where(genre_id: params[:genre]).order(created_at: :desc)
+    else
+     @contents = Content.all
+   end
+ end
 
   def show
   	@genres = Genre.all
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,12 @@
 class FavoritesController < ApplicationController
 	def create
-		content = Content.find(params[:content_id])
-		favorite = current_user.favorites.new(content_id: content.id)
+		@content = Content.find(params[:content_id])
+		favorite = current_user.favorites.new(content_id: @content.id)
 		favorite.save
-		redirect_to content_path(content)
 	end
 	def destroy
-		content = Content.find(params[:id])
-		favorite = current_user.favorites.find_by(content_id: content.id)
+		@content = Content.find(params[:id])
+		favorite = current_user.favorites.find_by(content_id: @content.id)
 		favorite.destroy
-		redirect_to content_path(content)
 	end
 end
