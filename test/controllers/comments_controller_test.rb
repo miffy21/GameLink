c1f03d8c10fe519c4bd1c326bc345075d44ee91f
keita <onini21@gmail.com>
2020-02-24 14:51:00 +0000

    テスト修正。

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,6 +4,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
     get comments_create_url
     assert_response :success
+    
   end
 
   test "should get destroy" do
